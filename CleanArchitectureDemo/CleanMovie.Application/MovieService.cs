31abbdfc246577f5cfd506caa991d6b9e2743b28
Arthur Namu <ArthurN@hollywoodbets.net>
2023-03-17 11:07:39 +0300

    Create the startup project files in observing CA

---
+++ CleanArchitectureDemo/CleanMovie.Application/MovieService.cs
@@ -0,0 +1,26 @@
+﻿using CleanMovieDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        //Contructor dependecy injection
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public List<Movie> GetAllMovies()
+        {
+            var movies = _movieRepository.GetAllMovies();
+
+            return movies;
+        }
+    }
+}
---
+++ CleanArchitectureDemo/CleanMovie.Infrastructure/MovieRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanMovie.Application;
+using CleanMovieDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infrastructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie{Id = 1, Name = "Passion Of Christ"},
+            new Movie{Id = 2, Name = "Home Alone"},
+        };
+        public List<Movie> GetAllMovies()
+        {
+            return  movies;
+        }
+    }
+}
